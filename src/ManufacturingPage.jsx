81728448641d0a993a20582a669c0ae88e0a285f
Nehajha1-cloud <nehajha@sandconsole.com>
2025-05-15 18:02:17 +0530

    update

--- src/ManufacturingPage.jsx
+++ src/ManufacturingPage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./ManufacturingPage.css";
 
 import Pipe from './assets/Image/Pipes.jpg';
+import Gear from './assets/logo/Gear.png';
+import Calander from './assets/logo/calander.png';
 
 const ManufacturingPage = () => {
   return (
@@ -21,7 +23,7 @@ const ManufacturingPage = () => {
 
       <div>
       <span className="rating">
-        <span className="stars">★★★★★</span>  {/* Golden stars */}
+        <span className="stars">★ ★ ★ ★ ★</span>  {/* Golden stars */}
         5.0
         <br />
         <span className="light">from 80+</span>
@@ -48,40 +50,42 @@ const ManufacturingPage = () => {
         <div className="card tall image-card">
           <img src={Pipe} alt="Pipes" />
         </div>
+
         <div className="card medium dark-card">
           <h2>100+</h2>
           <p>Our Esteemed Clients and Partners</p>
         </div>
+
         <div className="card short white-card">
-          <h3>Total Projects <span className="bold">↑ 3%</span></h3>
-          <h2>1951+</h2>
-          <p className="green">Increase of <span className="bold">126 </span>this month</p>
-        </div>
+
+  <div className="icon-row">
+    <img src={Calander} alt="calander" className="calander" />
+    <div className="dot-menu">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+  <h3>
+    Total Projects 
+    <span className="percent-change">
+      <span className="arrow-circle">↗</span> 8%
+    </span>
+  </h3>
+  <h2>1951+</h2>
+  <p className="green">Increase of <span className="bold">126 </span>this month</p>
+</div>
+
         <div className="card medium light-green-card">
           <h2>6+</h2>
           <p>Years of Dedicated Service</p>
         </div>
+
         <div className="card tall darks-card">
+          <img src={Gear} alt="gear" className="small-img"/>
           <p>Achieve Optimal Efficiency and Boost Productivity</p>
         </div>
       </div>
-
- {/* <div className="icon-wrapper">
-  <div className="icon left">
-    <div className="circle-btn">↗</div>
-    <div className="circle-btn">✦</div>
-  </div>
-
-  <div className="icon right">
-    <div className="circle-btn">📊</div>
-    <div className="circle-btn">〰️</div>
-  </div>
-  </div>  */}
-
-
-  
-
-
 
     </div> 
   );
